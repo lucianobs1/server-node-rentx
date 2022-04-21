066a26dbfafbc2c74292a56233c711596e670460
Luciano Brandao <lucianotetsu@gmail.com>
2022-04-21 07:32:18 -0300

    added feature create category

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,28 @@
+import { Category } from '../model/Category';
+
+interface ICategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[] = [];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
