0e16bdaa5daefe236bd33aa52b126956ede1996b
Luciano Brandao <lucianotetsu@gmail.com>
2022-04-26 18:46:39 -0300

    refactored createCategory useCase

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -9,6 +9,14 @@ class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
   async execute({ name, description }: IRequest): Promise<void> {
+    const categoryAlreadyExists = await this.categoriesRepository.findByName(
+      name,
+    );
+
+    if (categoryAlreadyExists) {
+      throw new Error('category exists');
+    }
+
     await this.categoriesRepository.create({
       name,
       description,
