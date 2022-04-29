d7aa6cda471b30fba56323fe0410b46c2691b3e4
Luciano Brandao <lucianotetsu@gmail.com>
2022-04-29 14:39:24 -0300

    added custom errors and refactored throw errors

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IRequest {
@@ -20,7 +21,7 @@ class CreateCategoryUseCase {
     );
 
     if (categoryAlreadyExists) {
-      throw new Error('category exists');
+      throw new AppError('category exists');
     }
 
     await this.categoriesRepository.create({
