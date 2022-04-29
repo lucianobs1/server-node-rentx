92622d35e65fc6d1d6c05a32f93b28fd5a72f26b
Luciano Brandao <lucianotetsu@gmail.com>
2022-04-28 21:24:08 -0300

    added injectable dependence and configs in listCategories and refactored async functions

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -4,11 +4,12 @@ import { container } from 'tsyringe';
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
 class ImportCategoryController {
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const importCategoryUseCase = container.resolve(ImportCategoryUseCase);
 
     const { file } = request;
-    importCategoryUseCase.execute(file);
+
+    await importCategoryUseCase.execute(file);
 
     return response.send();
   }
