9f18cd43fbdd468834f0099686431bc512b1eef8
lucianobs1 <lucianotetsu@gmail.com>
2022-08-08 22:59:07 -0300

    created middleware ensureAdmin

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,18 @@
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { Router } from 'express';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 
-carsRoutes.post('/', ensureAuthenticated, createCarController.handle);
+carsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle,
+);
 
 export { carsRoutes };
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,6 +1,7 @@
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
 import { Router } from 'express';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const specificationRoutes = Router();
@@ -10,6 +11,7 @@ const createSpecificationController = new CreateSpecificationController();
 specificationRoutes.post(
   '/',
   ensureAuthenticated,
+  ensureAdmin,
   createSpecificationController.handle,
 );
 
