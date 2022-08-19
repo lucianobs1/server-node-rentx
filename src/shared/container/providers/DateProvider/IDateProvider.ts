5504d5107298fcaaa028145cb33e40e5d1fccfae
lucianobs1 <lucianotetsu@gmail.com>
2022-08-19 12:50:30 -0300

    created feature resetPassword

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -5,6 +5,7 @@ interface IDateProvider {
   dateNow(): Date;
   addDays(day: number): Date;
   addHours(hours: number): Date;
+  compareIfBefore(start_date: Date, end_date: Date): boolean;
 }
 
 export { IDateProvider };
